6925bcfd420a0bb18918690e21159a7b28057ca7
Ayvie4305 <vieraatuhairwe729@gmail.com>
2023-04-05 12:23:17 +0300

    concatenates n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strncat - concatenates 2 strings using n bytes
+* @dest: destination string
+* @src: source string
+* @n: number of characters to concatenate
+* Return: destination string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1 = 0;
+	int len2 = 0;
+	int i;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		len1++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		len2++;
+	for (i = 0 ; i < n ; i++)
+		dest[len1 + i] = src[i];
+	return (dest);
+}
